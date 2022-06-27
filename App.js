8b0314bdb7422da1e94b0909b50a863046831472
loganrun <bigdog_41@hotmai.com>
2022-06-27 13:31:33 -0700

    navigation added

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MainLayout } from './src/screens';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={'Dashboard'}>
+
+        <Stack.Screen name="Dashboard" component={MainLayout}/>
+
+      </Stack.Navigator>
+      <StatusBar style="auto"/>
+    </NavigationContainer>
+    
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
